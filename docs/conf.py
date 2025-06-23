fa0213b614138f8ee8f79ebc780b10d5a454e9fc
zy-fang <47031619+zy-fang@users.noreply.github.com>
2025-06-23 20:38:58 +0800

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'scLinguist'
+copyright = '2025, Min Li Lab'
+author = 'Min Li Lab'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'nbsphinx',
+]
+nbsphinx_execute = 'never'
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
---
+++ docs/index.rst
@@ -0,0 +1,35 @@
+.. scLinguist documentation master file, created by
+   sphinx-quickstart on Mon Jun 23 17:48:48 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+scLinguist documentation
+===============================
+
+``scLinguist`` aims to xxx. 
+
+.. figure:: _static/Figure1_V2.tiff
+   :width: 80%
+   :align: center
+
+   Overview of scLinguist workflow
+
+Getting started with scLinguist
+------------------------------
+
+To begin using **scLinguist**, please refer to the following sections of the documentation:
+
+- The `Installation <./install.rst>`_ provides instructions for setting up **scLinguist** in your environment.
+
+- The `Tutorials <./tutorials/index.rst>`_ contains examples on how to use **scLinguist**.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+   :hidden:
+
+   install
+   tutorials/index
+   dataset
+   credits
+
